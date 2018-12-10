72329c63cf43f284aacae15a96fab6a1e62d8807
Fa <ngomfa@gmail.com>
2018-12-10 16:28:04 +0100

    exo3

---
+++ user.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Partie7_Exo3</title>
+</head>
+<body>
+  <?php
+
+    echo $_GET['nom'] . ' ' . $_GET['prenom'];
+
+// $_GET et $_POST sont des tableaux de données associatifs et superglobaux.
+//La méthode GET (celle qui est utilisée par défaut si rien n'est renseigné) fait circuler les informations du formulaire en clair dans la barre d'adresse.
+
+    ?>
+  </body>
+  </html>
